7cb4c7da244238c29a6423f14867a34e5412d3d5
lkook <lucasheinrich.kook@uzh.ch>
2023-03-20 18:47:44 +0100

    nop

--- code/nonparametric-dte.R
+++ code/nonparametric-dte.R
@@ -1,7 +1,7 @@
 # Nonparametric DTE estimation
 # LK March 2023
 
-set.seed(0)
+# set.seed(0)
 
 # dependencies ------------------------------------------------------------
 
@@ -9,11 +9,11 @@ library("deeptrafo")
 
 # DGP ---------------------------------------------------------------------
 
-gen_dat <- function(n = 1e4, p = 2, nz = 1, betaX = c(1, 0), betaY = c(-1, 0)) {
-  X <- matrix(rnorm(n * p), ncol = p, nrow = n)
+gen_dat <- function(n = 5e4, p = 2, nz = 1, betaX = c(1, 0), betaY = c(1, 0)) {
+  X <- matrix(runif(n * p), ncol = p, nrow = n)
   props <- plogis(X %*% betaX)
-  D <- as.numeric(props <= runif(n))
-  Y <- X %*% betaY + D + rnorm(n)
+  D <- as.numeric(props >= runif(n))
+  Y <- X %*% betaY + D + sqrt(exp(X %*% betaY)) * rnorm(n)
   structure(data.frame(Y = Y, D = D, X = X), betaX = betaX, betaY = betaY,
             props = props * D + (1 - props) * (1 - D))
 }
@@ -22,27 +22,36 @@ gen_dat <- function(n = 1e4, p = 2, nz = 1, betaX = c(1, 0), betaY = c(-1, 0)) {
 
 d <- gen_dat()
 dp <- gen_dat()
+dt <- gen_dat()
 
 # Fit models --------------------------------------------------------------
 
 ### Model for untreated
-m0 <- BoxCoxNN(Y | te(X.1, X.2) ~ 1, data = d[d$D == 0, ])
-fit(m0, epochs = 1e3, early_stopping = TRUE)
+# m0 <- BoxCoxNN(Y | X.1 + X.2 ~ 1, data = d[d$D == 0, ])
+# fit(m0, epochs = 1e3, early_stopping = TRUE)
+m0 <- Lm(Y ~ X.1 + X.2 | X.1 + X.2, data = d[d$D == 0, ])
 
 ### Model for treated
-m1 <- BoxCoxNN(Y | te(X.1, X.2) ~ 1, data = d[d$D == 1, ])
-fit(m1, epochs = 1e3, early_stopping = TRUE)
+# m1 <- BoxCoxNN(Y | X.1 + X.2 ~ 1, data = d[d$D == 1, ])
+# fit(m1, epochs = 1e3, early_stopping = TRUE)
+m1 <- Lm(Y ~ X.1 + X.2 | X.1 + X.2, data = d[d$D == 1, ])
 
 ### Predictions
 qs <- seq(max(min(d$Y[d$D == 0]), min(d$Y[d$D == 1])),
           min(max(d$Y[d$D == 0]), max(d$Y[d$D == 1])), length.out = 1e2)
-p0 <- do.call("cbind", predict(m0, type = "cdf", newdata = d[d$D == 0, -1], q = qs))
-p1 <- do.call("cbind", predict(m1, type = "cdf", newdata = d[d$D == 1, -1], q = qs))
+# p0 <- do.call("cbind", predict(m0, type = "cdf", newdata = dt[dt$D == 0, -1], q = qs))
+# p1 <- do.call("cbind", predict(m1, type = "cdf", newdata = dt[dt$D == 1, -1], q = qs))
+p0 <- t(predict(m0, newdata = dt[dt$D == 0, -1], q = qs, type = "distribution"))
+p1 <- t(predict(m1, newdata = dt[dt$D == 1, -1], q = qs, type = "distribution"))
+
+pp0 <- t(predict(m0, newdata = dp[dp$D == 0, -1], q = qs, type = "distribution"))
+pp1 <- t(predict(m1, newdata = dp[dp$D == 1, -1], q = qs, type = "distribution"))
 
 ### Propensity scores
-mp <- glm(D ~ X.1 + X.2, data = dp, family = "binomial")
-preds <- predict(mp, newdata = d[, -1], type = "response")
-ps <- d$D * preds + (1 - d$D) * (1 - preds)
+# mp <- glm(D ~ X.1 + X.2, data = dp, family = "binomial")
+# preds <- predict(mp, newdata = d[, -1], type = "response")
+# ps <- d$D * preds + (1 - d$D) * (1 - preds)
+ps <- attr(dt, "props")
 
 ### Influence function
 IF <- function(y, preds, Y, prop) {
@@ -50,12 +59,13 @@ IF <- function(y, preds, Y, prop) {
   (1 / prop) * (inds - preds)
 }
 
-IF0 <- sapply(seq_along(qs), \(idx) IF(qs[idx], p0[, idx], d$Y[d$D == 0], ps[d$D == 0]))
-IF1 <- sapply(seq_along(qs), \(idx) IF(qs[idx], p1[, idx], d$Y[d$D == 1], ps[d$D == 1]))
+IF0 <- sapply(seq_along(qs), \(idx) IF(qs[idx], pp0[, idx], dp$Y[dp$D == 0], ps[dp$D == 0]))
+IF1 <- sapply(seq_along(qs), \(idx) IF(qs[idx], pp1[, idx], dp$Y[dp$D == 1], ps[dp$D == 1]))
 
 # Plot --------------------------------------------------------------------
 
-plot(qs, colMeans(p1) - colMeans(p0) + colMeans(IF1) - colMeans(IF0), type = "s")
-dd <- \(y, dat) pnorm(y, mean = dat$D + dat$X.1 * attr(dat, "betaY")[1])
-lines(qs, sapply(qs, \(q) mean(dd(q, d[d$D == 1,])) - mean(dd(q, d[d$D == 0, ]))), col = 2)
-lines(qs, colMeans(p1) - colMeans(p0), type = "s", col = 3)
+dd <- \(y, dat) pnorm(y, mean = dat$D + dat$X.1 * attr(dat, "betaY")[1], sd = sqrt(exp(dat$X.1 * attr(dat, "betaY")[1])))
+plot(qs, sapply(qs, \(q) mean(dd(q, dt[dt$D == 1,])) - mean(dd(q, dt[dt$D == 0, ]))), col = 2, type = "l")
+lines(qs, colMeans(p1) - colMeans(p0), type = "l", col = 3)
+lines(qs, colMeans(p1) + colMeans(IF1) - colMeans(p0) - colMeans(IF0), type = "l")
+legend("bottomright", c("One-step", "Oracle", "Plug-in"), col = c(1, 2, 3), lwd = 1)
