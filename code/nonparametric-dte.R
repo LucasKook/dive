76341f343b6b88c6e57ffb39c4ed23f8d9e3bea6
lkook <lucasheinrich.kook@uzh.ch>
2023-03-21 16:09:25 +0100

    np advice

--- code/nonparametric-dte.R
+++ code/nonparametric-dte.R
@@ -33,31 +33,31 @@ dt <- gen_dat()
 # Fit models --------------------------------------------------------------
 
 ### Model for untreated
-# m0 <- BoxCoxNN(Y | X.1 + X.2 ~ 1, data = d[d$D == 0, ])
-# fit(m0, epochs = 1e3, early_stopping = TRUE)
-m0 <- Lm(Y ~ X.1 + X.2 | X.1 + X.2, data = d[d$D == 0, ])
+m0 <- BoxCoxNN(Y | X.1 + X.2 ~ 1, data = d[d$D == 0, ])
+fit(m0, epochs = 1e3, early_stopping = TRUE)
+# m0 <- Lm(Y ~ X.1 + X.2 | X.1 + X.2, data = d[d$D == 0, ])
 
 ### Model for treated
-# m1 <- BoxCoxNN(Y | X.1 + X.2 ~ 1, data = d[d$D == 1, ])
-# fit(m1, epochs = 1e3, early_stopping = TRUE)
-m1 <- Lm(Y ~ X.1 + X.2 | X.1 + X.2, data = d[d$D == 1, ])
+m1 <- BoxCoxNN(Y | X.1 + X.2 ~ 1, data = d[d$D == 1, ])
+fit(m1, epochs = 1e3, early_stopping = TRUE)
+# m1 <- Lm(Y ~ X.1 + X.2 | X.1 + X.2, data = d[d$D == 1, ])
 
 ### Predictions
 qs <- seq(max(min(d$Y[d$D == 0]), min(d$Y[d$D == 1])),
           min(max(d$Y[d$D == 0]), max(d$Y[d$D == 1])), length.out = 1e2)
-# p0 <- do.call("cbind", predict(m0, type = "cdf", newdata = dt[dt$D == 0, -1], q = qs))
-# p1 <- do.call("cbind", predict(m1, type = "cdf", newdata = dt[dt$D == 1, -1], q = qs))
-p0 <- t(predict(m0, newdata = dt[dt$D == 0, -1], q = qs, type = "distribution"))
-p1 <- t(predict(m1, newdata = dt[dt$D == 1, -1], q = qs, type = "distribution"))
-
-pp0 <- t(predict(m0, newdata = dp[dp$D == 0, -1], q = qs, type = "distribution"))
-pp1 <- t(predict(m1, newdata = dp[dp$D == 1, -1], q = qs, type = "distribution"))
+p0 <- do.call("cbind", predict(m0, type = "cdf", newdata = dt[dt$D == 0, -1], q = qs))
+p1 <- do.call("cbind", predict(m1, type = "cdf", newdata = dt[dt$D == 1, -1], q = qs))
+# p0 <- t(predict(m0, newdata = dt[dt$D == 0, -1], q = qs, type = "distribution"))
+# p1 <- t(predict(m1, newdata = dt[dt$D == 1, -1], q = qs, type = "distribution"))
+#
+# pp0 <- t(predict(m0, newdata = dp[dp$D == 0, -1], q = qs, type = "distribution"))
+# pp1 <- t(predict(m1, newdata = dp[dp$D == 1, -1], q = qs, type = "distribution"))
 
 ### Propensity scores
 # mp <- glm(D ~ X.1 + X.2, data = dp, family = "binomial")
 # preds <- predict(mp, newdata = d[, -1], type = "response")
 # ps <- d$D * preds + (1 - d$D) * (1 - preds)
-ps <- attr(dp, "props")
+ps <- attr(dt, "props")
 
 ### Influence function
 IF <- function(y, preds, Y, prop) {
@@ -65,8 +65,8 @@ IF <- function(y, preds, Y, prop) {
   (1 / prop) * (inds - preds)
 }
 
-IF0 <- sapply(seq_along(qs), \(idx) IF(qs[idx], pp0[, idx], dp$Y[dp$D == 0], ps[dp$D == 0]))
-IF1 <- sapply(seq_along(qs), \(idx) IF(qs[idx], pp1[, idx], dp$Y[dp$D == 1], ps[dp$D == 1]))
+IF0 <- sapply(seq_along(qs), \(idx) IF(qs[idx], p0[, idx], dt$Y[dt$D == 0], ps[dt$D == 0]))
+IF1 <- sapply(seq_along(qs), \(idx) IF(qs[idx], p1[, idx], dt$Y[dt$D == 1], ps[dt$D == 1]))
 
 # Plot --------------------------------------------------------------------
 
