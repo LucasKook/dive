62e47a62f274cb5fdd665c89cdd80614ddfa0402
lkook <lucasheinrich.kook@uzh.ch>
2023-03-22 14:09:03 +0100

    nop

--- code/random-forest-dte.R
+++ code/random-forest-dte.R
@@ -1,7 +1,6 @@
 
 library("tram")
 library("survival")
-library("trtf")
 library("ranger")
 library("randomForest")
 
@@ -37,7 +36,6 @@ summary(tn$predictions[1,])
 
 learn <- sample.int(nrow(GBSG2), size = floor(0.8 * nrow(GBSG2)))
 
-
 crf <- randomForest(time ~ horTh + age + menostat + tsize + tgrade + pnodes +
                       progrec + estrec, data = GBSG2[learn,])
 
@@ -53,13 +51,5 @@ pcdf <- Vectorize(\(y, idx) c(t(rfw[, idx]) %*% as.numeric(GBSG2$time[learn] <= 
 ts <- seq(min(GBSG2$time), max(GBSG2$time), length.out = 1e2)
 plot(ts, colMeans(pcdf(ts, idx0)), type = "l")
 lines(ts, colMeans(pcdf(ts, idx1)), type = "l", col = 2)
-plot(ecdf(GBSG2$time[GBSG2$horTh == "no"]), add = TRUE, col = rgb(.1, .1, .1, .1))
-plot(ecdf(GBSG2$time[GBSG2$horTh == "yes"]), add = TRUE, col = rgb(.9, .1, .1, .1))
-
-# m0 <- Coxph(surv ~ 1, data = GBSG2, prob = c(0.001, 0.999))
-# ctrl <- ctree_control(minsplit = 50, minbucket = 20, mincriterion = 0)
-# tf <- traforest(m0, formula = surv ~ horTh, control = ctrl, ntree = 50,
-#                 mtry = 1, trace = TRUE, data = GBSG2)
-#
-# predict(tf, newdata = data.frame(surv = 1:10, horTh = unique(GBSG2$horTh)),
-#         type = "distribution")
+plot(ecdf(GBSG2$time[GBSG2$horTh == "no"]), add = TRUE, col = rgb(.1, .1, .1, .1), pch = 20)
+plot(ecdf(GBSG2$time[GBSG2$horTh == "yes"]), add = TRUE, col = rgb(.9, .1, .1, .1), pch = 20)
