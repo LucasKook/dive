de1b4acb46346fffeac2c7dbc9097e819866adae
lkook <lucasheinrich.kook@uzh.ch>
2023-04-14 11:51:25 +0200

    fix ctrl fun

--- pkg/inst/scripts/control-rf.R
+++ pkg/inst/scripts/control-rf.R
@@ -31,18 +31,32 @@ gen_dat <- function(n = 1e3, doD = FALSE, nfine = 1e6) {
   data.frame(Y = Y, D = D, Z = Z, H = H)
 }
 
-rfk <- function(rf, data, idx) {
+# rfk <- function(rf, data, idx) {
+#   preds <- predict(rf, data = d, type = "terminalNodes")
+#   prox <- matrix(nrow = nrow(d), ncol = nrow(d))
+#   for (i in idx) {
+#     for (j in (1:nrow(d))[-idx]) {
+#       prox[i, j] <- mean(preds$predictions[i, ] == preds$predictions[j, ])
+#     }
+#   }
+#   rfw <- prox[idx,][,-idx]
+#   rfw / matrix(pmax(colSums(rfw), .Machine$double.eps),
+#                ncol = nrow(d) - length(idx),
+#                nrow = length(idx), byrow = TRUE)
+# }
+rfk <- function(rf, data) {
   preds <- predict(rf, data = d, type = "terminalNodes")
-  prox <- matrix(nrow = nrow(d), ncol = nrow(d))
-  for (i in idx) {
-    for (j in (1:nrow(d))[-idx]) {
-      prox[i, j] <- mean(preds$predictions[i, ] == preds$predictions[j, ])
+  inbag <- simplify2array(rf$inbag.counts)
+  rfw <- matrix(nrow = nrow(d), ncol = nrow(d))
+  for (i in 1:nrow(d)) {
+    for (j in (1:nrow(d))) {
+      tree_idx <- inbag[i, ] == 0 & inbag[j, ] == 0
+      rfw[i, j] <- sum(preds$predictions[i, tree_idx] ==
+                          preds$predictions[j, tree_idx]) / sum(tree_idx)
     }
   }
-  rfw <- prox[idx,][,-idx]
   rfw / matrix(pmax(colSums(rfw), .Machine$double.eps),
-               ncol = nrow(d) - length(idx),
-               nrow = length(idx), byrow = TRUE)
+               ncol = nrow(d), nrow = nrow(d), byrow = TRUE)
 }
 
 # Run ---------------------------------------------------------------------
@@ -56,23 +70,22 @@ test <- seq_len(nrow(d))[-learn]
 # cf <- glm(D ~ Z, data = d[learn,], family = "binomial")
 # preds <- predict(cf, type = "response", newdata = d)
 # d$ps <- d$D - 1 - preds
-cf <- ranger(D ~ Z, data = d[learn,], probability = TRUE)
+cf <- ranger(D ~ Z, data = d, probability = TRUE)
 preds <- dive:::.clip(predict(cf, data = d)$predictions)
 d$ps <- d$D - 1 - preds[, 2]
 
 # coef(lm(Y ~ D + ps, data = d[-learn,]))["D"]
 
 ### Fit RF with control function prediction and compute RF weights for prediction
-rf <- ranger(Y ~ D + ps, data = d[test,])
-rfw <- rfk(rf, d, test)
-
+rf <- ranger(Y ~ D + ps, data = d, keep.inbag = TRUE)
+rfw <- rfk(rf, d)
 
 # Predict and plot --------------------------------------------------------
 
-idx0 <- which(d$D[learn] == 0)
-idx1 <- which(d$D[learn] == 1)
+idx0 <- which(d$D == 0)
+idx1 <- which(d$D == 1)
 
-pcdf <- Vectorize(\(y, idx) c(t(rfw[, idx]) %*% as.numeric(d$Y[test] <= y)), "y")
+pcdf <- Vectorize(\(y, idx) c(t(rfw[, idx]) %*% as.numeric(d$Y <= y)), "y")
 ts <- seq(min(d$Y), max(d$Y), length.out = 1e2)
 
 gt <- Vectorize(\(y, idx, m = 0) pnorm(y, mean = m + d$H[idx]), "y")
--- pkg/inst/scripts/example1.R
+++ pkg/inst/scripts/example1.R
@@ -13,7 +13,7 @@ library("coin")
 
 gen_dat <- function(n = 1e3, doD = FALSE, nfine = 1e6) {
   ### Instrument
-  Z <- sample(0:1, n, TRUE) # rt(n, df = 5)
+  Z <- sample(c(-1, 1), n, TRUE) # rt(n, df = 5)
   ### Hidden
   fH <- rt(nfine, df = 5)
   H <- rt(n, df = 5)
@@ -21,7 +21,7 @@ gen_dat <- function(n = 1e3, doD = FALSE, nfine = 1e6) {
   # gD <- (1 - doD) * fH + rlogis(nfine)
   # UD <- dive:::.clip(ecdf(gD)((1 - doD) * H + rlogis(n)))
   UD <- runif(n)
-  D <- as.numeric(plogis(Z + doD * H) >= UD)
+  D <- as.numeric(plogis(Z + (1 - doD) * H) >= UD)
   ### Response
   # gY <- fH + rnorm(nfine)
   # UY <- dive:::.clip(ecdf(gY)(H + rnorm(n)))
@@ -31,18 +31,19 @@ gen_dat <- function(n = 1e3, doD = FALSE, nfine = 1e6) {
   data.frame(Y = Y, D = D, Z = Z, H = H)
 }
 
-rfk <- function(rf, data, idx) {
+rfk <- function(rf, data) {
   preds <- predict(rf, data = d, type = "terminalNodes")
-  prox <- matrix(nrow = nrow(d), ncol = nrow(d))
-  for (i in idx) {
-    for (j in (1:nrow(d))[-idx]) {
-      prox[i, j] <- mean(preds$predictions[i, ] == preds$predictions[j, ])
+  inbag <- simplify2array(rf$inbag.counts)
+  rfw <- matrix(nrow = nrow(d), ncol = nrow(d))
+  for (i in 1:nrow(d)) {
+    for (j in (1:nrow(d))) {
+      tree_idx <- inbag[i, ] == 0 & inbag[j, ] == 0
+      rfw[i, j] <- sum(preds$predictions[i, tree_idx] ==
+                          preds$predictions[j, tree_idx]) / sum(tree_idx)
     }
   }
-  rfw <- prox[idx,][,-idx]
   rfw / matrix(pmax(colSums(rfw), .Machine$double.eps),
-               ncol = nrow(d) - length(idx),
-               nrow = length(idx), byrow = TRUE)
+               ncol = nrow(d), nrow = nrow(d), byrow = TRUE)
 }
 
 cor_obj <- \(b, Y, X, E) {
@@ -59,44 +60,75 @@ ind_obj <- \(b, Y, X, E, tstat = "quadratic", trafo = identity) {
 
 op1 <- integrate(\(x) plogis(1 + x) * dt(x, df = 5), -20, 20)$value
 op0 <- integrate(\(x) plogis(x) * dt(x, df = 5), -20, 20)$value
-oATE <- op1 - op0
-oOR <- (op1 * (1 - op0)) / ((1 - op1) * op0)
 
-### Non-collapsibility
-# coef(glm(Y ~ D, data = gen_dat(1e5, doD = TRUE), family = "binomial"))["D"]
+### Oracle ATE (collapsible)
+(oATE <- op1 - op0)
 
-# Run ---------------------------------------------------------------------
+### Oracle OR (non-collapsible)
+(oOR <- (op1 * (1 - op0)) / ((1 - op1) * op0))
+
+# Sanity checks -----------------------------------------------------------
+
+# Data under intervention on D (d0) and observational (d)
+d0 <- gen_dat(1e5, doD = TRUE)
+d1 <- gen_dat(1e5, doD = FALSE)
+
+# causal OR b/c nonparametric (everything binary)
+exp(coef(mD <- glm(Y ~ D, data = d0, family = "binomial"))["D"])
+diff(predict(mD, newdata = data.frame(D = c(0, 1)), type = "response"))
+# conditional causal OR
+# exp(coef(mDH <- glm(Y ~ D + H, data = d0, family = "binomial"))["D"])
+# diff(predict(mDH, newdata = data.frame(D = c(1, 0), H = 0), type = "response"))
+
+### Under doD should return the causal OR
+exp(COR0 <- optim(c(0, 0), cor_obj, Y = d0$Y, X = cbind(1, d0$D), E = d0$Z)$par)[2]
+exp(IND0 <- optim(c(0, 0), ind_obj, Y = d0$Y, X = cbind(1, d0$D), E = d0$Z)$par)[2]
+
+### Even under obs should return the causal OR
+exp(COR <- optim(c(0, 0), cor_obj, Y = d1$Y, X = cbind(1, d1$D), E = d1$Z)$par)[2]
+exp(IND <- optim(c(0, 0), ind_obj, Y = d1$Y, X = cbind(1, d1$D), E = d1$Z)$par)[2]
+
+### Naive control function (parametric, breaks down for more complex examples)
+S1 <- glm(D ~ Z, data = d1, family = "binomial")
+d1$R <- d1$D - predict(S1, type = "response")
+S2 <- glm(Y ~ D + R, data = d1, family = "binomial")
+NCTL <- unname(exp(coef(S2)[2]))
+
+# Nonparametric control function ------------------------------------------
 
 ### Generate data
-d <- gen_dat()
-learn <- seq_len(floor(nrow(d) / 2) + 1)
-test <- seq_len(nrow(d))[-learn]
-
-### COR and IND
-optim(c(0, 0), cor_obj, Y = d$Y, X = cbind(1, d$D), E = d$Z)$par
+# learn <- seq_len(floor(nrow(d) / 2) + 1)
+# test <- seq_len(nrow(d))[-learn]
 
 ### Fit RF for control function
-cf <- ranger(D ~ Z, data = d[learn,], probability = TRUE)
-preds <- dive:::.clip(predict(cf, data = d)$predictions)
-d$ps <- d$D - 1 - preds[, 2]
+d <- d1[1:1e3,]
+dZ <- d1[-1:-1e3,]
+cf <- ranger(D ~ Z, data = dZ, probability = TRUE)
+preds <- predict(cf, data = d)$predictions
+d$ps <- d$D - preds[, 1]
+# cf <- glm(D ~ Z, data = dZ, family = "binomial")
+# preds <- predict(cf, newdata = d, type = "response")
+# d$ps <- d$D - preds
 
 ### Fit RF with control function prediction and compute RF weights for prediction
-rf <- ranger(Y ~ D + ps, data = d[test,], probability = TRUE)
-rfw <- rfk(rf, d, test)
+rf <- ranger(Y ~ D + ps, data = d, keep.inbag = TRUE)
+rfw <- rfk(rf, d)
 
-# Predict and plot --------------------------------------------------------
+# Results -----------------------------------------------------------------
 
-idx0 <- which(d$D[learn] == 0)
-idx1 <- which(d$D[learn] == 1)
+idx0 <- which(d$D == 0)
+idx1 <- which(d$D == 1)
 
-pcdf <- Vectorize(\(y, idx) c(t(rfw[, idx]) %*% as.numeric(d$Y[test] <= y)), "y")
+pcdf <- Vectorize(\(y, idx) c(t(rfw[, idx]) %*% as.numeric(d$Y <= y)), "y")
 
 p0 <- colMeans(pcdf(0, idx0))
 p1 <- colMeans(pcdf(0, idx1))
 
 ### ATE
-c(ORACLE = oATE, CF = p0 - p1, COR = NA, IND = NA)
+DM <- matrix(c(1, 0, 1, 1), nrow = 2, byrow = TRUE)
+c(ORACLE = oATE, CF = p0 - p1, COR = diff(plogis(DM %*% COR)),
+  IND = diff(plogis(DM %*% IND)))
 
 ### OR
 c(ORACLE = oOR, CF = 1 / ((p1 * (1 - p0)) / ((1 - p1) * p0)),
-  COR = NA, IND = NA)
+  COR = exp(COR[2]), IND = exp(IND[2]), NCTL = NCTL)
