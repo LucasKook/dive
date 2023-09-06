46785e201ee0e058b5a93d5daaef33d72c4a8750
lkook <lucasheinrich.kook@uzh.ch>
2023-09-06 17:01:10 +0200

    nop

--- inst/scripts/example1-continuous.R
+++ inst/scripts/example1-continuous.R
@@ -11,18 +11,45 @@ devtools::load_all()
 
 # Data --------------------------------------------------------------------
 
-n <- 1e4
+n <- 1e3
 
 # Data under intervention on D (d0) and observational (d)
-d0 <- dgp_ex1_cont(n, doD = TRUE)
-d1 <- dgp_ex1_cont(n, doD = FALSE)
+# d0 <- dgp_ex1_cont(n, doD = TRUE)
+# d1 <- dgp_ex1_cont(n, doD = FALSE)
+
+dgp <- function(n = 1e3, doD = FALSE, cf = rnorm(5)) {
+  ### Instrument
+  Z <- rt(n, df = 5) # sample(0:1, n, TRUE) # rt(n, df = 5)
+  ### Hidden
+  H <- rt(n, df = 5)
+  ### Treatment
+  UD <- runif(n)
+  D <- as.numeric(plogis(cf[1] + cf[2] * Z + cf[3] * (1 - doD) * H) >= UD)
+  ### Response
+  UY <- runif(n)
+  Y <- qnorm(UY, mean = cf[4] * D + cf[5] * H, sd = 1 + abs(D + H))
+  ### Return
+  ret <- data.frame(Y = Y, D = D, Z = Z, H = H)
+
+  ### Compute oracle
+  oracle_distr <- Vectorize(\(y, d = 0) {
+    integrate(\(x) pnorm(y, mean = d + x, sd = 1 + abs(d + x)) * dt(x, df = 5),
+              -20, 20)$value
+  }, "y")
+
+  structure(ret, odist = oracle_distr, cf = cf)
+}
+
+# d0 <- dgp(n, doD = TRUE)
+d1 <- dgp(n, doD = FALSE)
+d1t <- dgp(n, doD = FALSE)
 
 # plot(ecdf(d0$Y[d0$D == 0]), add = TRUE, lty = 2, cex = 0.1)
 # plot(ecdf(d0$Y[d0$D == 1]), add = TRUE, col = 2, lty = 2, cex = 0.1)
 
 oracle_distr <- attr(d1, "odist")
 
-ys <- seq(min(d0$Y), max(d0$Y), length.out = 1e3)
+ys <- seq(min(d1$Y), max(d1$Y), length.out = 1e3)
 F1 <- oracle_distr(ys, d = 1)
 F0 <- oracle_distr(ys, d = 0)
 
@@ -33,24 +60,25 @@ lines(ys, F1, type = "l", col = 2, lty = 2)
 
 ### Fit RF for control function
 cf <- ranger(factor(D) ~ Z, data = d1, probability = TRUE)
-preds <- predict(cf, data = d1)$predictions
-# d1$ps <- d1$D - preds[, 2]
-d1$ps <- randomized_pit(preds[, 1], d1$D, trafo = qnorm)
+preds <- predict(cf, data = d1t)$predictions
+d1t$V <- preds[, 1] * (d1t$D == 0) + preds[, 2] * (d1t$D == 1)
+# d1$V <- d1$D - preds[, 2]
+# d1$V <- randomized_pit(preds[, 1], d1$D, trafo = qnorm)
 
 ### Fit RF with control function prediction and compute RF weights for prediction
-rf <- ranger(Y ~ D + ps, data = d1, quantreg = TRUE)
+rf <- ranger(Y ~ D + V, data = d1t, quantreg = TRUE)
 
 # TRAM --------------------------------------------------------------------
 
-# m0 <- BoxCox(Y ~ ps, data = d1, subset = d1$D == 0, order = 20)
-# m1 <- BoxCox(Y ~ ps, data = d1, subset = d1$D == 1, order = 20)
+# m0 <- BoxCox(Y ~ V, data = d1t, subset = d1$D == 0, order = 20)
+# m1 <- BoxCox(Y ~ V, data = d1t, subset = d1$D == 1, order = 20)
 #
-# lines(ys, rowMeans(predict(m0, type = "distribution", newdata = d1[,-1], q = ys)), lty = 3)
-# lines(ys, rowMeans(predict(m1, type = "distribution", newdata = d1[,-1], q = ys)), lty = 3, col = 2)
+# lines(ys, rowMeans(predict(m0, type = "distribution", newdata = d1t[,-1], q = ys)), lty = 1)
+# lines(ys, rowMeans(predict(m1, type = "distribution", newdata = d1t[,-1], q = ys)), lty = 1, col = 2)
 
 # Results -----------------------------------------------------------------
 
-nd0 <- nd1 <- d1
+nd0 <- nd1 <- d1t
 nd0$D <- 0
 nd1$D <- 1
 p0 <- predict(rf, data = nd0, quantiles = qs <- seq(0, 1, length.out = 1e3), type = "quantiles")$pred
