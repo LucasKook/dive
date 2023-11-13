1bf2c09986352e37b0a0ade828bf52a7a402a5af
lkook <lucasheinrich.kook@uzh.ch>
2023-11-13 15:08:59 +0100

    max loss

--- inst/scripts/indep-unif-loss.R
+++ inst/scripts/indep-unif-loss.R
@@ -2,6 +2,7 @@
 # LK 2023
 
 set.seed(42)
+save <- FALSE
 
 # DEPs --------------------------------------------------------------------
 
@@ -36,75 +37,75 @@ ys <- seq(min(ws$Y), max(ws$Y), length.out = 3e2)
 nd0 <- data.frame(Y = ys, D = factor(0, levels = c(0, 1)))
 nd1 <- data.frame(Y = ys, D = factor(1, levels = c(0, 1)))
 
-n <- 3e2
-nsim <- 5
-xis <- 0.5
-for (txi in xis) {
-  out <- list()
-  for(iter in 1:nsim) {
+n <- 3e3
+nsim <- 1
+out <- list()
+for(iter in 1:nsim) {
 
-    set.seed(111 * iter)
+  set.seed(111 * iter)
 
-    ### Generate data
-    d <- dgp(n)
+  ### Generate data
+  d <- dgp(n)
 
-    topt <- trafo_control(order_bsp = 30L, support = range(ws$Y),
-                          response_type = "continuous")
-    ### Optional warm start, use with care
-    # m0 <- ColrNN(Y | D ~ 1, data = d, optimizer = optimizer_adam(0.1),
-    #              trafo_options = topt, tf_seed = iter)
-    # fit(m0, validation_split = 0, batch_size = 1e4, epochs = 1e4, callbacks = list(
-    #   callback_reduce_lr_on_plateau("loss", patience = 20, factor = 0.5),
-    #   callback_early_stopping("loss", patience = 50)))
+  topt <- trafo_control(order_bsp = 30L, support = range(ws$Y),
+                        response_type = "continuous")
+  ### Optional warm start, use with care
+  # m0 <- ColrNN(Y | D ~ 1, data = d, optimizer = optimizer_adam(0.1),
+  #              trafo_options = topt, tf_seed = iter)
+  # fit(m0, validation_split = 0, batch_size = 1e4, epochs = 1e4, callbacks = list(
+  #   callback_reduce_lr_on_plateau("loss", patience = 20, factor = 0.5),
+  #   callback_early_stopping("loss", patience = 50)))
 
-    ### Setup model with independence loss and warm start
-    m <- ColrDA(Y | D ~ 1, anchor = ~ Z, data = d, loss = "indep",
-                optimizer = optimizer_adam(0.1), trafo_options = topt,
-                tf_seed = 11 * iter, xi = txi)
-    # set_weights(m$model, get_weights(m0$model))
+  ### Setup model with independence loss and warm start
+  m <- ColrDA(Y | D ~ 1, anchor = ~ Z, data = d, loss = "indep",
+              optimizer = optimizer_adam(0.1), trafo_options = topt,
+              tf_seed = 11 * iter)
+  # set_weights(m$model, get_weights(m0$model))
 
-    ### Fit
-    fit(m, epochs = 1e4)
+  ### Fit
+  fit(m, epochs = 1e4)
 
-    ### Diagnostics
-    preds <- predict(m, type = "cdf")
-    pp <- ggplot(data.frame(pr = preds), aes(x = pr)) +
-      stat_ecdf() +
-      geom_abline(intercept = 0, slope = 1, linetype = 2) +
-      theme_bw() +
-      labs(caption = parse(text = paste0("xi==", txi)),
-           subtitle = paste0("F(Y, D) indep Z: p-value = ", round(
-             dHSIC::dhsic.test(preds, d$Z, method = "gamma")$p.value, 4)))
-    print(pp)
-    ggsave(paste0("inst/ignore/ecdf-xi-", txi, "-iter-", iter, ".pdf"), pp, height = 5, width = 7)
-
-    ### Return
-    out[[iter]] <- data.frame(
-      iter = iter, ys = ys,
-      p0 = predict(m, newdata = nd0, type = "cdf"),
-      p1 = predict(m, newdata = nd1, type = "cdf"))
-  }
-
-  dint <- dgp(n = 2e4, TRUE)
-  dobs <- dgp(n = 2e4)
-  pdat <- bind_rows(out) |>
-    pivot_longer(p0:p1, names_to = "trt", values_to = "cdf") |>
-    mutate(trt = factor(trt, levels = c("p0", "p1"), labels = c(0, 1)))
-  mdat <- pdat |> group_by(ys, trt) |> summarize(cdf = mean(cdf))
-
-  px <- ggplot(pdat, aes(x = ys, y = cdf, color = trt, group = interaction(trt, iter))) +
-    geom_line(alpha = 0.3, aes(linetype = "Est."), show.legend = FALSE) +
-    geom_line(aes(linetype = "Est.", group = trt), data = mdat, lwd = 0.8) +
+  ### Diagnostics
+  preds <- predict(m, type = "cdf")
+  pp <- ggplot(data.frame(pr = preds), aes(x = pr)) +
+    stat_ecdf() +
+    geom_abline(intercept = 0, slope = 1, linetype = 2) +
     theme_bw() +
-    stat_ecdf(aes(x = Y, color = D, linetype = "Interv."), data = dint,
-              inherit.aes = FALSE, lwd = 0.8, geom = "line") +
-    stat_ecdf(aes(x = Y, color = D, linetype = "Observ."), data = dobs,
-              inherit.aes = FALSE, lwd = 0.8, geom = "line") +
-    scale_color_manual(values = c("0" = "darkblue", "1" = "darkred")) +
-    labs(x = "Y", y = "CDF", color = "Treatment", linetype = "Type",
-         caption = parse(text = paste0("xi==", txi))) +
-    theme(legend.position = "top", text = element_text(size = 12.5))
-  print(px)
+    labs(caption = parse(text = paste0("xi==", txi)),
+         subtitle = paste0("F(Y, D) indep Z: p-value = ", round(
+           dHSIC::dhsic.test(preds, d$Z, method = "gamma")$p.value, 4)))
+  print(pp)
+  if (save)
+    ggsave(paste0("inst/ignore/ecdf-xi-", txi, "-iter-", iter, ".pdf"), pp,
+           height = 5, width = 7)
 
-  ggsave(paste0("inst/ignore/iu-xi-", txi, ".pdf"), px, height = 5, width = 7)
+  ### Return
+  out[[iter]] <- data.frame(
+    iter = iter, ys = ys,
+    p0 = predict(m, newdata = nd0, type = "cdf"),
+    p1 = predict(m, newdata = nd1, type = "cdf"))
 }
+
+dint <- dgp(n = 2e4, TRUE)
+dobs <- dgp(n = 2e4)
+pdat <- bind_rows(out) |>
+  pivot_longer(p0:p1, names_to = "trt", values_to = "cdf") |>
+  mutate(trt = factor(trt, levels = c("p0", "p1"), labels = c(0, 1)))
+mdat <- pdat |> group_by(ys, trt) |> summarize(cdf = mean(cdf))
+
+px <- ggplot(pdat, aes(x = ys, y = cdf, color = trt, group = interaction(trt, iter))) +
+  geom_line(alpha = 0.3, aes(linetype = "Est."), show.legend = FALSE) +
+  geom_line(aes(linetype = "Est.", group = trt), data = mdat, lwd = 0.8) +
+  theme_bw() +
+  stat_ecdf(aes(x = Y, color = D, linetype = "Interv."), data = dint,
+            inherit.aes = FALSE, lwd = 0.8, geom = "line") +
+  stat_ecdf(aes(x = Y, color = D, linetype = "Observ."), data = dobs,
+            inherit.aes = FALSE, lwd = 0.8, geom = "line") +
+  scale_color_manual(values = c("0" = "darkblue", "1" = "darkred")) +
+  labs(x = "Y", y = "CDF", color = "Treatment", linetype = "Type",
+       caption = parse(text = paste0("xi==", txi))) +
+  theme(legend.position = "top", text = element_text(size = 12.5))
+print(px)
+
+if (save)
+  ggsave(paste0("inst/ignore/iu-xi-", txi, ".pdf"), px, height = 5, width = 7)
