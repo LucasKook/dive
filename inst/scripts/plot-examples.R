d96f0301b06fec9cb057c002081d16381314ca61
lkook <lucasheinrich.kook@uzh.ch>
2024-02-05 17:07:05 +0100

    plot

--- inst/scripts/plot-examples.R
+++ inst/scripts/plot-examples.R
@@ -8,7 +8,7 @@ theme_set(theme_bw() + theme(text = element_text(size = 13.5)))
 
 # DGP ---------------------------------------------------------------------
 
-setting <- c("rinv-violated", "rsim-violated", "cmrs-violated")[3]
+setting <- c("rinv-violated", "rsim-violated", "cmrs-violated", "intro")[4]
 if (setting == "cmrs-violated") {
   tg0 <- \(h, ny) h
   tg1 <- \(h, ny) -h
@@ -22,6 +22,10 @@ if (setting == "cmrs-violated") {
 } else if (setting == "rsim-violated") {
   tg0 <- \(h, ny) h^2
   tg1 <- \(h, ny) -h^2
+  tqH <- \(h) 0.2 + 0.6 * as.numeric(h > 0)
+} else if (setting == "intro") {
+  tg0 <- \(h, ny) 3 * cos(-1 + h + ny)
+  tg1 <- \(h, ny) 3 * sin(-1 + h + ny)
   tqH <- \(h) 0.2 + 0.6 * as.numeric(h > 0)
 }
 
@@ -82,3 +86,54 @@ punif <- ggplot(pd, aes(x = W)) +
 
 ggsave(paste0("inst/figures/rank-assumptions", setting, ".pdf"),
        width = 12, height = 3.5)
+
+
+# Intro plot --------------------------------------------------------------
+
+do <- dgp_np(3e4, intervene = FALSE)
+di <- dgp_np(3e4, intervene = TRUE)
+pdat <- bind_rows(do |> mutate(setting = "Observational"),
+                  di |> mutate(setting = "Interventional"))
+
+p1 <- ggplot(pdat, aes(x = Y, color = factor(D))) +
+  stat_ecdf() +
+  facet_grid(~ setting) +
+  scale_color_brewer(palette = "Dark2") +
+  labs(x = "Response", y = "CDF", color = "Treatment", tag = "A") +
+  theme(legend.position = "top")
+
+# qte <- \(p, d) quantile(d$Y1, probs = p) - quantile(d$Y0, probs = p)
+# rte <- \(y, d) log(ecdf(d$Y1)(y)) - log(ecdf(d$Y0)(y))
+# rpd <- \(p, d) ecdf(d$Y1)(quantile(d$Y0, probs = p)) - p
+dte <- \(y, d) ecdf(d$Y[d$D == 1])(y) - ecdf(d$Y[d$D == 0])(y)
+tte <- \(y, qZ = \(p) log(p) - log(1 - p), d) {
+  ret <- qZ(ecdf(d$Y[d$D == 1])(y)) - qZ(ecdf(d$Y[d$D == 0])(y))
+  ret[is.infinite(ret)] <- NA
+  ret
+}
+dok <- \(y, d) quantile(d$Y[d$D == 0], probs = ecdf(d$Y[d$D == 1])(y)) - y
+ys <- seq(-3 + sqrt(.Machine$double.eps), 3 - sqrt(.Machine$double.eps),
+          length.out = 1e3)
+FUNs <- c("Distributional" = "dte", "Logit-transformed" = "tte", "Doksum-type" = "dok")
+make_plot_data <- \(td) {
+  out <- bind_cols(lapply(FUNs, \(x) {
+    do.call(x, list(y = ys, d = td))
+  }))
+  colnames(out) <- names(FUNs)
+  out$y <-ys
+  out |> pivot_longer(-y)
+}
+
+pdat2 <- bind_rows(make_plot_data(do) |> mutate(setting = "Observational"),
+                   make_plot_data(di) |> mutate(setting = "Interventional"))
+
+p2 <- ggplot(pdat2, aes(x = y, y = value, color = setting)) +
+  geom_line() +
+  facet_wrap(~ name, scales = "free") +
+  scale_color_manual(values = colorspace::diverge_hcl(2)) +
+  labs(x = "Response", y = "Treatment effect", color = "Data", tag = "B") +
+  theme(legend.position = "top")
+
+ggpubr::ggarrange(p1, p2, widths = c(2, 3))
+
+ggsave("inst/figures/intro.pdf", height = 3, width = 9)
