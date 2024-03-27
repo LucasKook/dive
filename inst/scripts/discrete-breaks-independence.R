881648aceed267f1c754e07ddc2b84c495d2c7d6
Lucas Kook <lucasheinrich.kook@gmail.com>
2024-03-27 16:45:51 +0100

    ex

--- inst/scripts/discrete-breaks-independence.R
+++ inst/scripts/discrete-breaks-independence.R
@@ -48,3 +48,38 @@ ipit <- predict(dive, type = "cdf")
 plot(ecdf(ipit))
 abline(0, 1)
 cor.test(ipit, d$z)
+
+### EX WITH NP
+### Example discrete breaks independence
+### LK 2024
+
+set.seed(12)
+
+# Data --------------------------------------------------------------------
+
+dgp <- function(n = 1e2) {
+  z <- as.numeric(runif(n) > 0.5)
+  x <- as.numeric(z + rlogis(n) > 0)
+  y <- as.numeric(-1 + x + rlogis(n) > 0)
+  data.frame(y = y, x = x, z = z)
+}
+
+obj <- \(b, Y, X, E, lambda = 1) {
+  Xb <- X %*% b
+  p0 <- 1 - (Xb >= 0) * (Xb <= 1) * Xb
+  R <- randomized_pit(p0, Y)
+  cmv <- mean((R - ecdf(R)(R))^2)
+  cor <- mean(fitted(lm(R ~ E))^2)
+  max(lambda * cor, cmv)
+}
+
+d <- dgp()
+ps <- seq(0, 1, length.out = 1e2)
+bs <- data.frame(expand.grid(p0 = ps, p1 = ps))
+
+loss <- apply(bs, 1, \(tb) obj(b = tb, Y = d$y, X = cbind(1, d$x), E = d$z, lambda = 0))
+obj(c(plogis(-1), 0.5 - plogis(-1)), Y = d$y, X = cbind(1, d$x), E = d$z, lambda = 0)
+min(loss)
+
+c(plogis(-1), plogis(0))
+bs[which.min(loss), ]
