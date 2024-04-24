0feb38d0ef8213ed1d09b4fd9261125052b84418
Lucas Kook <lucasheinrich.kook@gmail.com>
2024-04-24 14:13:53 +0200

    sim

--- inst/scripts/simulations.R
+++ inst/scripts/simulations.R
@@ -26,29 +26,29 @@ dgp <- switch(
   "1" = function(n = 1e2, do = FALSE) {
     Z <- rlogis(n)
     H <- rlogis(n)
-    D <- as.numeric(3 * Z + (1 - do) * 4 * H > rlogis(n))
-    Y <- 4 * D + 6 * H
+    D <- as.numeric(4 * Z + (1 - do) * 4 * H > rlogis(n))
+    Y <- -10 + 8 * D + 6 * H
     data.frame(Y = Y, H = H, D = D, Z = Z)
   },
   "2" = function(n = 1e2, do = FALSE) {
     Z <- rlogis(n)
     H <- rlogis(n)
-    D <- as.numeric(3 * Z + (1 - do) * 4 * H > rlogis(n))
-    Y <- 4 * D + 6 * H + 2 * H * rlogis(n)
+    D <- as.numeric(4 * Z + (1 - do) * 4 * H > rlogis(n))
+    Y <- 16 * D + 6 * H + 2 * H * rlogis(n)
     data.frame(Y = Y, H = H, D = D, Z = Z)
   },
   "3" = function(n = 1e2, do = FALSE) {
     Z <- rlogis(n)
     H <- rlogis(n)
-    D <- as.numeric(3 * Z + (1 - do) * 4 * H > rlogis(n))
+    D <- as.numeric(4 * Z + (1 - do) * 4 * H > rlogis(n))
     Y <- log(1 + exp(18 + 8 * D + 6 * H))
     data.frame(Y = Y, H = H, D = D, Z = Z)
   },
   "4" = function(n = 1e2, do = FALSE) {
     Z <- rlogis(n)
     H <- rlogis(n)
-    D <- as.numeric(3 * Z + (1 - do) * 4 * H > rlogis(n))
-    Y <- - 4 * (1 + D) * H
+    D <- as.numeric(4 * Z + (1 - do) * 4 * H > rlogis(n))
+    Y <- - 4 * (1 + 3 * D) * H
     data.frame(Y = Y, H = H, D = D, Z = Z)
   }
 )
@@ -63,60 +63,90 @@ oracle <- Vectorize(\(y, d) d * F1(y) + (1 - d) * F0(y))
 # Params ------------------------------------------------------------------
 
 nep <- 1e4
-rep <- 20
+rep <- 2
 ords <- c(10, 30, 50)
 lrs <- c(0.01, 0.05, 0.1)
 ns <- c(1e2, 3e2, 7e2, 1e3)
-lams <- c(0, 0.5, 1, 3, 10)
+
+# FUNs --------------------------------------------------------------------
+
+fit_adaptive <- function(
+    args, epochs, max_iter = 5, stepsize = 2, alpha = 0.1, ...
+) {
+  for (iter in seq_len(max_iter)) {
+    mod <- do.call("ColrDA", c(args, list(tf_seed = iter)))
+    tmp <- get_weights(mod$model)
+    tmp[[1]][] <- qlogis(seq(0.0001, 0.9999, length.out = length(tmp[[1]])))
+    tmp[[2]][] <- - seq_len(length(tmp[[2]])) + 0.5
+    tmp[[3]][] <- 4
+    set_weights(mod$model, tmp)
+    # plot(args$data$Y, predict(mod, type = "cdf"), col = args$data$D + 1)
+    fit(mod, epochs = epochs, ...)
+    iPIT <- predict(mod, type = "cdf")
+    unif <- ks.test(iPIT, "punif")$p.value
+    indep <- dHSIC::dhsic.test(iPIT, args$data$Z, method = "gamma")$p.value
+    mod$xi <- args$xi
+    mod$p.unif <- unif
+    mod$p.indep <- indep
+    if (min(unif, indep) > alpha)
+      return(mod)
+    else
+      args$xi <- ifelse(indep < unif, args$xi * (1 + stepsize),
+                        args$xi / (1 + stepsize))
+  }
+  message("No solution for which uniformity and independence is not
+          rejected at level alpha.")
+  return(do.call("ColrDA", args))
+}
+
+check_unif_indep <- function(iPIT, Z) {
+  plot(iPIT ~ Z)
+  c(unif = ks.test(iPIT, "punif")$p.value,
+    indep = dHSIC::dhsic.test(iPIT, Z, method = "gamma")$p.value)
+}
 
 # Run ---------------------------------------------------------------------
 
 res <- lapply(ns, \(tn) {
   lapply(ords, \(tord) {
     lapply(lrs, \(tlr) {
-      lapply(lams, \(tlam) {
-        lapply(seq_len(rep), \(iter) {
-          ### Generate train/test data
-          dat <- dgp(tn)
-          test <- dgp(tn)
+      lapply(seq_len(rep), \(iter) {
+        ### Generate data
+        dat <- dgp(tn)
 
-          ### Compute oracle interventional CDF
-          dat$ORACLE <- oracle(dat$Y, dat$D)
-          test$ORACLE <- oracle(test$Y, test$D)
+        ### Compute oracle interventional CDF
+        dat$ORACLE <- oracle(dat$Y, dat$D)
 
-          ### Fit vanilla nonparametric CDF
-          m0 <- BoxCox(Y | D ~ 1, data = dat,
-                       support = supp <- range(dat$Y, test$Y),
-                       order = tord)
+        ### Checks
+        # print(check_unif_indep(dat$ORACLE, dat$Z))
+        # print(check_unif_indep(ecdf(dat$Y)(dat$Y), dat$Z))
 
-          ### Fit DIVE
-          m <- BoxCoxDA(Y | D ~ 1, data = dat, anchor = ~ Z, loss = "indep",
-                        optimizer = optimizer_adam(tlr), xi = tlam,
-                        trafo_options = trafo_control(
-                          order_bsp = tord, support = supp))
-          fit(m, epochs = nep)
+        ### Fit vanilla nonparametric CDF
+        m0 <- Colr(Y | D ~ 1, data = dat, support = supp <- range(dat$Y),
+                   order = tord)
 
-          ### Evaluate in-sample
-          dat$TRAM <- c(predict(m0, which = "distribution",
-                                type = "distribution"))
-          dat$DIVE <- c(predict(m, type = "cdf"))
+        ### Fit DIVE
+        debugonce(fit_adaptive)
+        args <- list(formula = Y | D ~ 1, data = dat, anchor = ~ Z,
+                     loss = "indep", optimizer = optimizer_adam(tlr),
+                     xi = 1, trafo_options = trafo_control(
+                       order_bsp = tord, support = supp))
+        cb <- list(callback_reduce_lr_on_plateau("loss", patience = 2e2,
+                                                 factor = 0.9),
+                   callback_early_stopping("loss", patience = 4e2))
+        m <- fit_adaptive(args, nep, callbacks = cb)
 
-          ### Evaluate out-of-sample
-          test$TRAM <- c(predict(m0, which = "distribution",
-                                type = "distribution", newdata = test))
-          test$DIVE <- c(predict(m, type = "cdf", newdata = test))
+        ### Evaluate
+        dat$TRAM <- c(predict(m0, which = "distribution",
+                              type = "distribution"))
+        dat$DIVE <- c(predict(m, type = "cdf"))
 
-          out <- bind_rows(dat |> mutate(set = "train"),
-                           test |> mutate(set = "test"))
-
-          out |> pivot_longer(TRAM:DIVE, names_to = "method",
-                              values_to = "cdf") |>
-            group_by(method, set) |>
-            summarize(CmV = mean((ORACLE - cdf)^2),
-                      KS = max(abs(ORACLE - cdf))) |>
-            mutate(n = tn, lam = tlam, order = tord, lr = tlr, iter = iter)
-
-        }) |> bind_rows()
+        dat |> pivot_longer(TRAM:DIVE, names_to = "method",
+                            values_to = "cdf") |>
+          group_by(method) |>
+          summarize(CmV = mean((ORACLE - cdf)^2),
+                    KS = max(abs(ORACLE - cdf))) |>
+          mutate(n = tn, order = tord, lr = tlr, iter = iter)
       }) |> bind_rows()
     }) |> bind_rows()
   }) |> bind_rows()
@@ -124,13 +154,8 @@ res <- lapply(ns, \(tn) {
 
 # Vis ---------------------------------------------------------------------
 
-tuned <- res |>
-  pivot_wider(names_from = set, values_from = CmV:KS) |>
-  group_by(method, n, lam, order, lr, iter) |>
-  slice(which.min(CmV_train))
-
-ggplot(tuned |> pivot_longer(c(CmV_test, KS_test), names_to = "metric", values_to = "value"),
-       aes(x = ordered(n), y = value, fill = method)) +
+ggplot(res, aes(x = ordered(n), y = value, fill = method)) +
+  facet_wrap(~ metric) +
   geom_boxplot() +
   theme_bw()
 
