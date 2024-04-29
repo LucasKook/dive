286248ffcff00928ecb59abdd11cd0378fdc75f8
Lucas Kook <lucasheinrich.kook@gmail.com>
2024-04-29 15:06:16 +0200

    applications

--- inst/application/401k.R
+++ inst/application/401k.R
@@ -2,6 +2,7 @@
 ### LK 2024
 
 set.seed(12)
+save <- TRUE
 
 # DEPs --------------------------------------------------------------------
 
@@ -17,11 +18,13 @@ d401k <- read_csv("inst/data/401k.csv")
 dat <- data.frame(
   y = d401k$net_tfa / 1e3,
   d = factor(d401k$p401),
-  z = d401k$e401
+  z = factor(d401k$e401)
 )[sample.int(nrow(d401k), 1e3), ]
 
 idx <- which(dat$y == 0)
 dat$y[idx] <- runif(length(idx), max(dat$y[dat$y < 0]), min(dat$y[dat$y > 0]))
+
+dat$oy <- ordered(dat$y)
 
 # FUNs --------------------------------------------------------------------
 
@@ -29,15 +32,16 @@ fit_adaptive <- function(
     args, epochs, max_iter = 5, stepsize = 2, alpha = 0.1, ws = NULL, ...
 ) {
   for (iter in seq_len(max_iter)) {
-    mod <- do.call("ColrDA", c(args, list(tf_seed = iter)))
+    mod <- do.call("PolrDA", c(args, list(tf_seed = iter)))
     if (!is.null(ws))
       set_weights(mod$model, ws)
-    browser()
-    # plot(args$data$y, predict(mod, type = "cdf"), col = args$data$d)
+    plot(args$data$y, predict(mod, type = "cdf"), col = args$data$d)
     fit(mod, epochs = epochs, ...)
+    plot(args$data$y, predict(mod, type = "cdf"), col = args$data$d)
     iPIT <- predict(mod, type = "cdf")
     unif <- ks.test(iPIT, "punif")$p.value
-    indep <- dHSIC::dhsic.test(iPIT, args$data$z, method = "gamma")$p.value
+    indep <- dHSIC::dhsic.test(iPIT, as.numeric(args$data$z),
+                               method = "gamma")$p.value
     mod$xi <- args$xi
     mod$p.unif <- unif
     mod$p.indep <- indep
@@ -54,54 +58,59 @@ fit_adaptive <- function(
 
 # Run ---------------------------------------------------------------------
 
-tord <- 10
+tord <- 6
 
 ### Fit
 m0 <- BoxCox(y | 0 + d ~ 1, data = dat, support = range(dat$y), order = tord)
 
-### Warmstart with conditional distribution
-mtmp <- ColrNN(y | d ~ 1, data = dat, order = tord,
-               optimizer = optimizer_adam(0.1))
-fit(mtmp, epochs = 3e3, validation_split = 0, callbacks = list(
-  callback_reduce_lr_on_plateau("loss", factor = 0.9, patience = 20),
-  callback_early_stopping("loss", patience = 40)))
+### Initialization
+mtmp <- PolrNN(oy | d ~ 1, data = dat)
 tmp <- get_weights(mtmp$model)
-args <- list(formula = y | d ~ 1, data = dat, anchor = ~ z, loss = "indep",
-             optimizer = optimizer_adam(0.01), order = tord, xi = 0.001)
-cb <- list(callback_reduce_lr_on_plateau("loss", patience = 2e2, factor = 0.9),
-           callback_early_stopping("loss", patience = 4e2))
+tmp[[1]][] <- -4.5
+tmp[[2]][] <- -4.5
+tmp[[3]][] <- -1
+args <- list(formula = oy | d ~ 1, data = dat, anchor = ~ z, loss = "indep",
+             optimizer = optimizer_adam(0.1), xi = 1/3, tf_seed = 1)
+cb <- list(callback_reduce_lr_on_plateau("loss", patience = 2e1, factor = 0.9),
+           callback_early_stopping("loss", patience = 6e1))
 m <- fit_adaptive(args, 1e4, callbacks = cb, ws = tmp)
 
 ### Predict
-dat$TRAM <- predict(m0, which = "distribution", type = "distribution")
+dat$Nonparametric <- predict(m0, which = "distribution", type = "distribution")
 dat$DIVE <- predict(m, type = "cdf")
 
-pdat <- dat |> pivot_longer(TRAM:DIVE, names_to = "model", values_to = "rank")
+pdat <- dat |> pivot_longer(Nonparametric:DIVE, names_to = "model", values_to = "rank")
+
+# Vis ---------------------------------------------------------------------
 
 p1 <- ggplot(pdat, aes(x = rank, color = factor(z))) +
   geom_abline(intercept = 0, slope = 1, linetype = 3, color = "gray40") +
   facet_wrap(~ model) +
   stat_ecdf() +
   scale_color_brewer(palette = "Dark2") +
-  labs(x = "PIT rank", y = "ECDF") +
+  labs(x = "Estimated iPIT", y = "ECDF", color = "401(k) eligibility") +
   theme_bw() +
   theme(text = element_text(size = 13.5))
 
-nd <- expand_grid(d = sort(unique(dat$d)), y = seq(
-  min(dat$y), max(dat$y), length.out = 1e3))
-nd$TRAM <- predict(m0, type = "distribution", newdata = nd)
+nd <- expand_grid(d = sort(unique(dat$d)), y = unique(dat$y))
+nd$oy <- ordered(nd$y)
+nd$Nonparametric <- predict(m0, type = "distribution", newdata = nd)
 nd$DIVE <- predict(m, type = "cdf", newdata = nd)
 
-p2 <- ggplot(nd |> pivot_longer(TRAM:DIVE, names_to = "model", values_to = "cdf"),
+p2 <- ggplot(nd |> pivot_longer(Nonparametric:DIVE, names_to = "model", values_to = "cdf"),
        aes(x = y, y = cdf, color = factor(d))) +
   geom_rug(aes(x = y), data = dat, inherit.aes = FALSE, color = "gray80",
            alpha = 0.1) +
   facet_wrap(~ model) +
-  geom_line() +
-  labs(x = "log(wage)", y = "CDF") +
+  geom_step() +
+  labs(x = "Net total financial assets", y = "Estimated CDF", color = "401(k) participation") +
   theme_bw() +
-  theme(text = element_text(size = 13.5))
+  theme(text = element_text(size = 13.5)) +
+  scale_color_manual(values = colorspace::diverge_hcl(2))
 
 ggpubr::ggarrange(p2, p1, ncol = 1, align = "hv")
 
-ggsave("inst/figures/401k.pdf", height = 6, width = 7)
+# Save --------------------------------------------------------------------
+
+if (save)
+  ggsave("inst/figures/401k.pdf", height = 6, width = 8)
--- inst/scripts/schooling.R
+++ inst/scripts/schooling.R
@@ -19,29 +19,29 @@ dat <- SchoolingReturns[sample.int(nrow(SchoolingReturns), 1e3), ]
 
 # Run ---------------------------------------------------------------------
 
+### Nonparametric
 m0 <- BoxCox(wage | smsa ~ 1, data = dat, support = range(dat$wage))
-plot(m0, which = "distribution", newdata = data.frame(
-  smsa = sort(unique(SchoolingReturns$smsa))), col = 1:2)
 
+### DIVE
 m <- BoxCoxDA(wage | smsa ~ 1, data = dat, anchor = ~ nearcollege,
-              loss = "indep", optimizer = optimizer_adam(0.05),
+              loss = "indep", optimizer = optimizer_adam(0.01),
               order = 10, xi = 1, tf_seed = 1)
 fit(m, epochs = 1e4)
-
-plot(m, newdata = data.frame(smsa = sort(unique(SchoolingReturns$smsa))),
-     type = "cdf")
 
 dat$Nonparametric <- predict(m0, which = "distribution", type = "distribution")
 dat$DIVE <- predict(m, type = "cdf")
 
-pdat <- dat |> pivot_longer(Nonparametric:DIVE, names_to = "model", values_to = "rank")
+pdat <- dat |>
+  pivot_longer(Nonparametric:DIVE, names_to = "model", values_to = "rank")
+
+# Vis ---------------------------------------------------------------------
 
 p1 <- ggplot(pdat, aes(x = rank, color = nearcollege)) +
   geom_abline(intercept = 0, slope = 1, linetype = 3, color = "gray40") +
   facet_wrap(~ model) +
   stat_ecdf() +
   scale_color_brewer(palette = "Dark2") +
-  labs(x = "PIT rank", y = "ECDF") +
+  labs(x = "Estimated iPIT", y = "ECDF", color = "Near college") +
   theme_bw() +
   theme(text = element_text(size = 13.5))
 
@@ -50,17 +50,21 @@ nd <- expand_grid(smsa = sort(unique(dat$smsa)), wage = seq(
 nd$Nonparametric <- predict(m0, type = "distribution", newdata = nd)
 nd$DIVE <- predict(m, type = "cdf", newdata = nd)
 
-p2 <- ggplot(nd |> pivot_longer(Nonparametric:DIVE, names_to = "model", values_to = "cdf"),
-       aes(x = wage, y = cdf, color = smsa)) +
+p2 <- ggplot(
+  nd |> pivot_longer(Nonparametric:DIVE, names_to = "model", values_to = "cdf"),
+  aes(x = wage, y = cdf, color = smsa)) +
   geom_rug(aes(x = wage), data = dat, inherit.aes = FALSE, color = "gray80",
            alpha = 0.1) +
   facet_wrap(~ model) +
   geom_line() +
-  labs(x = "log(wage)", y = "CDF") +
+  labs(x = "log(wage)", y = "Estimated CDF", color = "Metropolitan area") +
   theme_bw() +
-  theme(text = element_text(size = 13.5))
+  theme(text = element_text(size = 13.5)) +
+  scale_color_manual(values = colorspace::diverge_hcl(2))
 
 ggpubr::ggarrange(p2, p1, ncol = 1, align = "hv")
+
+# Save --------------------------------------------------------------------
 
 if (save)
   ggsave("inst/figures/case-study.pdf", height = 6, width = 7)
