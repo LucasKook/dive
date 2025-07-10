ac3537d5949606947428ed9d55787154edaa6fe3
Lucas Kook <lucasheinrich.kook@gmail.com>
2025-07-10 15:49:25 +0200

    include absdev

--- inst/code/vis-coverage.R
+++ inst/code/vis-coverage.R
@@ -8,6 +8,7 @@ save <- TRUE
 
 devtools::load_all()
 library("tidyverse")
+library("ggpubr")
 library("tram")
 library("dare")
 
@@ -30,22 +31,32 @@ pd <- dat |>
   summarize(
     const = sum((ORACLE * (1 - ORACLE))[-1] * diff(Y)),
     cover = sum(((lwr <= ORACLE & upr >= ORACLE) * ORACLE * (1 - ORACLE) / const)[-1] * diff(Y)),
-    se = cover * (1 - cover) / sqrt(300),
-    lwr = cover - 2 * se,
-    upr = cover + 2 * se
+    absdev = sum((abs((lwr <= ORACLE & upr >= ORACLE) - 0.8) * ORACLE * (1 - ORACLE) / const)[-1] * diff(Y))
   )
 
-ggplot(pd, aes(x = ordered(n), y = cover, color = method)) +
+p1 <- ggplot(pd, aes(x = ordered(n), y = cover, color = method)) +
   ggbeeswarm::geom_quasirandom(aes(shape = factor(D)), alpha = 0.1) +
   stat_summary(size = rel(0.5), fun.data = mean_se, fun.args = list(mult = 0)) +
   geom_hline(yintercept = 0.8, color = "darkred", linetype = 2) +
   theme_bw() +
-  labs(x = "Sample size", y = "Pointwise coverage", color = "Method", shape = "Treatment") +
+  labs(x = "Sample size", y = "Average coverage", color = "Method", shape = "Treatment", tag = "A") +
   lims(y = c(0, 1)) +
   theme(legend.position = "top", text = element_text(size = 13.5)) +
   scale_color_brewer(palette = "Dark2") +
   scale_shape_manual(values = c("0" = 4, "1" = 5))
 
+p2 <- ggplot(pd, aes(x = ordered(n), y = absdev, color = method)) +
+  ggbeeswarm::geom_quasirandom(aes(shape = factor(D)), alpha = 0.1) +
+  stat_summary(size = rel(0.5), fun.data = mean_se, fun.args = list(mult = 0)) +
+  theme_bw() +
+  labs(x = "Sample size", y = "Average absolute deviation", color = "Method", shape = "Treatment", tag = "B") +
+  lims(y = c(0, 1)) +
+  theme(legend.position = "top", text = element_text(size = 13.5)) +
+  scale_color_brewer(palette = "Dark2") +
+  scale_shape_manual(values = c("0" = 4, "1" = 5))
+
+ggarrange(p1, p2, align = "hv", common.legend = TRUE)
+
 if (save) {
-  ggsave("inst/figures/sim-coverage.pdf", height = 4.5, width = 5.5)
+  ggsave("inst/figures/sim-coverage.pdf", height = 4.5, width = 11)
 }
